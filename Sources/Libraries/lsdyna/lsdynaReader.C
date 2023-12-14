c9ac8af696c515d305f9ca0b654d73d9afb1d117
luciano_inti-mswin <luciano.buglioni@gmail.com>
2023-12-14 09:14:43 -0300

    Working with LS-DYNA

--- Sources/Libraries/lsdyna/lsdynaReader.C
+++ Sources/Libraries/lsdyna/lsdynaReader.C
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -17,24 +18,106 @@ bool isComment(const string &line){
   // std::remove_if(str.begin(), str.end(), [](char chr){ return chr == '&' || chr == ' ';}),
   // str.end());
 
+int findNextCommandLine(const int &curr_line, std::vector <std::string> line) {
+  string first;
+  int i = curr_line;
+  bool end = false;
+  while (!end) {
+    i++;
+    if (i==line.size()) end = true; 
+      first = line[i].substr(0,1);
+      //cout << "first "<<first <<", "<<line[i]<<endl;
+      //Can a line start with spaces??
+      //if (first.find("$")!=string::npos)
+      if (first=="*"){
+        //cout << "FOUND * at line "<<i<<endl;
+        end = true;
+      }
+  }// while end
+  
+        //cout << "* NOT FOUND "<<i<<endl;
+  return i;
+}
+
+void lsdynaReader::removeComments(){
+  std::vector<std::string>::iterator it;
+  int i=0;
+  for (it = m_line.begin();it!=m_line.end();){
+    //cout << *it<<endl;
+    string first = it->substr(0,1);
+    //cout << first <<endl;
+    if (first=="$"){
+      m_line.erase(it);
+      m_line_count--;
+    } else {
+      it ++;
+      //cout << "NOT $ FOUND "<<first<<", LIN: "<<*it<<endl;
+    }
+    i++;
+  }
+}
+
+void lsdynaReader::readNodes() {
+  bool end = false;
+  int i = 0;
+  while (!end){
+
+      if (m_line[i].find("*NODE") != std::string::npos){
+        cout << "Node command found at line "<<m_line_count<<endl;
+        
+        m_node_count = findNextCommandLine(i,m_line) - i;
+        cout << "Node count: "<<m_node_count<<endl;
+        end = true;
+      }
+    if (i==m_line_count) {
+      end = true;
+      cout << "NODES not defined "<<endl;
+    }
+    i++;
+  } 
+  
+  for (i=0;i<m_node_count;i++){
+    int id;
+    ls_node nod();
+  }
+
+}  //line
+
+
 lsdynaReader::lsdynaReader(const char *fname){
   string line;
   m_line_count = 0;
   int start, end;
   char dl = ' ';
   ifstream file(fname);
-    if (file.is_open()) {
-        while (getline(file, line)) {
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      m_line.push_back(line);
+      m_line_count++;
+    }
+  }
+        file.close();
+  cout << "Line count: "<< m_line_count << endl;
+  
+  removeComments();
+  cout << "Line count w/o comments: "<< m_line_count << endl;
+  
+  readNodes();
+  
+  std::vector<std::string>::iterator it = m_line.begin();
+  for (int i=0;i<m_line_count;i++) {
           start = end = 0;
-          //cout << line << "\n";
+          //cout << m_line[i]<<endl;
           // start = line.find_first_not_of(dl, end);
           if (!isComment(line)){
-            if (line.find('*') != std::string::npos){
+            if (m_line[i].find('*') != std::string::npos){
               //cout << "COMMAND FOUND at "<< line.find("*")<<", line "<<m_line_count<<endl;
                 
             } 
-            if (line.find("*NODE") != std::string::npos){
+            if (m_line[i].find("*NODE") != std::string::npos){
               cout << "Node command found at line "<<m_line_count<<endl;
+              
+              cout << "Node count: "<<findNextCommandLine(i,m_line) - i<<endl;
             }
           } else {
             // cout << "Commented line "<<m_line_count<<endl;
@@ -42,10 +125,10 @@ lsdynaReader::lsdynaReader(const char *fname){
           // if (start != string::npos){
             
           // }
-          m_line_count++;
           
-        }
-        file.close();
-    }  
-  cout << "Line count: "<< m_line_count << endl;
+          
+    it++;
+
+    }  //line
+
 }
--- Sources/Libraries/lsdyna/lsdynaReader.h
+++ Sources/Libraries/lsdyna/lsdynaReader.h
@@ -2,12 +2,26 @@
 #define _LS_DYNA_READER_
 
 #include <string>
-
+#include <vector>
 
 class Keyword {
 public:
 
 
+};
+
+struct ls_node {
+  ls_nodeconst (const int &id_, const double &x, const double &y, const double &z){
+    m_id = id_;
+    m_x[0]=x;m_x[1]=y;m_x[2]=z;
+  }
+  int m_id;
+  double m_x[3];
+};
+
+struct ls_element {
+  int id;
+  std::vector <int> node;
 };
 
 class lsdynaReader{
@@ -16,7 +30,13 @@ public:
   lsdynaReader(const char *);
 protected:
   int m_line_count;
+  int m_node_count;
+  std::vector <std::string> m_line;
+  void readNodes();
+  void removeComments();
   
+  std::vector < ls_node    > m_node;
+  std::vector < ls_element > m_elem;
 };
 
 
